a3abe1230f3df430398a7bb7288c36b5ed2bc599
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-05-27 09:26:08 +0200

    OCP: new shape that is not supported

--- OCP/Program.cs
+++ OCP/Program.cs
@@ -8,10 +8,12 @@ namespace OCP
         {
             Circle c1 = new Circle() { Radius = 5 };
             Rectangle r1 = new Rectangle() { Width = 4, Height = 5 };
+            Triangle t1 = new Triangle() { Width = 3, Height = 7 };
             Geometry g = new Geometry();
 
             Console.WriteLine($"C1 area: {g.CalculateArea(c1)}");
             Console.WriteLine($"R1 area: {g.CalculateArea(r1)}");
+            Console.WriteLine($"T1 area: {g.CalculateArea(t1)}");
         }
     }
 }
---
+++ OCP/Triangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    class Triangle : IShape
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+    }
+}
