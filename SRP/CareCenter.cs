d700f1a5cdbe3b2d938c2c16c1bdc1977eb47905
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-05-27 08:49:49 +0200

    SRP: bad example

---
+++ SRP/CareCenter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    class CareCenter
+    {
+        /*
+          Stakeholder 1: Allow visits in person
+        */
+        public bool AcceptVisitor(DateTime visitTime)
+        {
+            return IsCareCenterOpen(visitTime);
+        }
+
+        /*
+         Stakeholder 2: Connect incoming call to nurse
+        */
+        public bool AcceptPhoneCall(DateTime visitTime)
+        {
+            return IsCareCenterOpen(visitTime);
+        }
+
+        private bool IsCareCenterOpen(DateTime datetime)
+        {
+            return datetime.Hour >= 8 && datetime.Hour <= 17;
+        }
+    }
+}
---
+++ SRP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CareCenter careCenter = new CareCenter();
+
+            // Stakeholder 1 use case
+            DateTime visitor1VisitTime = new DateTime(2021, 5, 27, 9, 0, 0);
+            Console.WriteLine($"Visitor 1 is allowed in: {careCenter.AcceptVisitor(visitor1VisitTime)}");
+
+            DateTime visitor2VisitTime = new DateTime(2021, 5, 27, 20, 0, 0);
+            Console.WriteLine($"Visitor 2 is allowed in: {careCenter.AcceptVisitor(visitor2VisitTime)}");
+
+            // Stakeholder 2 use case
+            DateTime callee1CallTime = new DateTime(2021, 5, 27, 10, 0, 0);
+            Console.WriteLine($"Callee 1 is allowed in: {careCenter.AcceptPhoneCall(callee1CallTime)}");
+
+            DateTime callee2CallTime = new DateTime(2021, 5, 27, 19, 0, 0);
+            Console.WriteLine($"Callee 2 is allowed in: {careCenter.AcceptPhoneCall(callee2CallTime)}");
+
+            Console.ReadKey();
+        }
+    }
+}
