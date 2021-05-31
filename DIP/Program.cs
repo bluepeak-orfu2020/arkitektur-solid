f7d4dc50b1cc63f384b4d398ca4f190d143b3d3c
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-05-31 10:35:59 +0200

    DIP: bad example

---
+++ DIP/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace DIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            string line;
+            while ((line = Console.ReadLine()) != "" )
+            {
+                string[] parts = line.Split(" ");
+                if (parts[0] == "+")
+                {
+                    int value1 = int.Parse(parts[1]);
+                    int value2 = int.Parse(parts[2]);
+                    Console.WriteLine($"Result: {calculator.Add(value1, value2)}");
+                }
+                else if (parts[0] == "-")
+                {
+                    int value1 = int.Parse(parts[1]);
+                    int value2 = int.Parse(parts[2]);
+                    Console.WriteLine($"Result: {calculator.Sub(value1, value2)}");
+                }
+                else if (parts[0] == "set")
+                {
+                    calculator.SetCounter();
+                    Console.WriteLine($"Counter set!");
+                }
+                else if (parts[0] == "inc")
+                {
+                    calculator.IncrementCounter();
+                    Console.WriteLine($"Result: {calculator.GetCounterValue()}");
+                }
+            }
+        }
+    }
+
+    class Calculator
+    {
+        private Counter counter;
+        public int Add(int i, int j)
+        {
+            return i + j;
+        }
+        
+        public int Sub(int i, int j)
+        {
+            return i - j;
+        }
+
+        public void SetCounter()
+        {
+            counter = new Counter();
+        }
+        
+        public void IncrementCounter()
+        {
+            counter.Increment();
+        }
+
+        public int GetCounterValue()
+        {
+            return counter.GetValue();
+        }
+    }
+
+    class Counter
+    {
+        private int x = 0;
+
+        public void Increment()
+        {
+            x++;
+        }
+
+        public int GetValue()
+        {
+            return x;
+        }
+    }
+}
