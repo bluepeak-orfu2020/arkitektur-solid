b524489a070daf484aa58ebe8b7f58ed5b47edea
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-05-31 10:04:34 +0200

    ISP: bad example

---
+++ ISP/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ISP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IHamburgerRestaurant restaurant = new JoesHamburgerRestaurant();
+            IHamburgerRestaurant restaurant = new BobbysHamburgerRestaurant();
+
+            restaurant.OrderHamburger();
+            restaurant.OrderFries();
+            restaurant.OrderSoda();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IHamburgerRestaurant
+    {
+        void OrderHamburger();
+
+        void OrderFries();
+
+        void OrderSoda();
+    }
+
+    class JoesHamburgerRestaurant : IHamburgerRestaurant
+    {
+        public void OrderHamburger()
+        {
+            Console.WriteLine("OrderHamburger");
+        }
+
+        public void OrderFries()
+        {
+            Console.WriteLine("OrderFries");
+        }
+
+        public void OrderSoda()
+        {
+            Console.WriteLine("OrderSoda");
+        }
+    }
+
+    class BobbysHamburgerRestaurant : IHamburgerRestaurant
+    {
+        public void OrderFries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderHamburger()
+        {
+            Console.WriteLine("OrderHamburger from Bobby");
+        }
+
+        public void OrderSoda()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
